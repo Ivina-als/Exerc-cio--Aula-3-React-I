2334773bee1d165bb5d820f2108b303d0aeeac4c
ivina albuquerque <ivina.albuquerque.s@gmail.com>
2023-01-18 01:20:49 -0300

    preparar para push

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//router é responsavel por gerenciar as rotas, routes engloba as rotas e route é a rota em si
-
-//criar portas: npm i react-router-dom -> instalou o react rounter dom
-
-//com o router será possível verificar se o usuário está autenticado antes de navegar por
-//outras páginas da minha aplicação
-
-// agora essa função app() vai só gerenciar as rotas das páginas
 import Login from "./pages/Login/index";
 import Home from "./pages/Home";
 function App() {
